931deb7a8ab6201aeda2597cd6d8d8120876e74b
Ahmed el bahi <81105228+bahi-ops@users.noreply.github.com>
2024-04-25 17:11:14 +0200

    Refactor SQL script to create procedure for computing average score and insert into average_scores table

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -1,9 +1,16 @@
--- 7. Average score
+-- creation of the procudeures 
+-- creation of an average function for the user to use in their own code.
+
 CREATE PROCEDURE ComputeAverageScoreForUser(IN user_id INT)
 BEGIN
-    DECLARE avg_score FLOAT;
+    DECLARE avg_score DECIMAL(10, 2);
+
     SELECT AVG(score) INTO avg_score
-    FROM corrections
-    WHERE user_id = ComputeAverageScoreForUser.user_id;
-    UPDATE users SET average_score = avg_score WHERE id = user_id;
-END;
+    FROM scores
+    WHERE user_id = user_id;
+
+    INSERT INTO average_scores (user_id, average_score)
+    VALUES (user_id, avg_score);
+END //
+
+DELIMITER ;
