5cdf0ae58a34b7d20c582bb1419fc88e0a86fd59
Ahmed el bahi <81105228+bahi-ops@users.noreply.github.com>
2024-04-26 21:02:56 +0300

    Optimize SQL script for counting fans by country of origin in 0x00-MySQL_Advanced/2-fans.sql

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,10 +1,15 @@
 -- Task: Write a SQL script that ranks country origins of bands, ordered by the number of (non-unique) fans
 
-SELECT 
-   b.country AS origin,
-   COUNT(DISTINCT f.fan_id) AS nb_fans
-FROM bands b
-JOIN band_members bm ON b.band_id = bm.band_id
-JOIN fans f ON bm.member_id = f.member_id
-GROUP BY b.country
+SELECT
+    country AS origin,
+    SUM(fans) AS nb_fans
+FROM
+    (SELECT
+         b.country,
+         COUNT(f.fan_id) AS fans
+     FROM bands b
+     JOIN band_members bm ON b.band_id = bm.band_id
+     JOIN fans f ON bm.member_id = f.member_id
+     GROUP BY b.band_id) AS subquery
+GROUP BY country
 ORDER BY nb_fans DESC;
