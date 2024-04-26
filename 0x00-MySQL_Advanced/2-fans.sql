f638fb38629d636241310c1babbc18b2d7e3d960
Ahmed el bahi <81105228+bahi-ops@users.noreply.github.com>
2024-04-26 20:53:18 +0300

    Refactor SQL script to optimize query for counting fans by country of origin in 0x00-MySQL_Advanced/2-fans.sql

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,7 +1,8 @@
 -- Assuming the table is named 'metal_bands'
+-- Write a query that returns the number of fans for each country of origin of the bands. The query should return the following columns:
 SELECT 
-    country AS origin,
-    COUNT(fan_id) AS nb_fans
+       country AS origin,
+       COUNT(fan_id) AS nb_fans
 FROM bands b
 JOIN band_members bm ON b.band_id = bm.band_id
 JOIN fans f ON bm.member_id = f.member_id
