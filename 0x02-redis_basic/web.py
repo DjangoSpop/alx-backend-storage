af8d25593054a2888c049d1e3686cc40881c3844
Ahmed el bahi <81105228+bahi-ops@users.noreply.github.com>
2024-05-01 14:07:26 +0300

    eeewww

---
+++ 0x02-redis_basic/web.py
@@ -0,0 +1,22 @@
+import redis
+import requests
+"""_summary_
+get page and use the count in redis
+to get the request has been made to
+the url
+"""
+def get_page(url:str)-> str:
+    url = 'http://slowwly.robertomurray.co.uk'
+    requests.get(url)
+    redis_client = redis.Redis()
+
+    # Increment the count for the URL
+    url_count_key = f"count:{url}"
+    redis_client.incr(url_count_key)
+
+    # Cache the result with an expiration time of 10 seconds
+    result_key = f"result:{url}"
+    redis_client.set(result_key, get_page(url))
+    redis_client.expire(result_key, 10)
+    
+
